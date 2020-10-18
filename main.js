aec8133418f6e2b93572c99a991d4f24c23b1a6b
markxoe <mark@oude-elberink.de>
2020-10-18 19:40:18 +0200

    :rewind: Removed Tray Icon

--- main.js
+++ main.js
@@ -12,19 +12,6 @@ const createWindow = () => {
         icon: "res/icon/ico-icon.ico",
     });
     mainWindow.loadFile("app/index.html");
-
-    const tray = new Tray(getIcon());
-    tray.setToolTip("Eine Biene fliegt daher");
-    console.log(app.isPackaged);
-};
-
-const getIcon = () => {
-    if (app.isPackaged) {
-        //return process.resourcesPath + "icon/ico-icon.ico";
-        return path.join(process.resourcesPath, "icon/ico-icon.ico");
-    } else {
-        return "res/icon/ico-icon.ico";
-    }
 };
 
 app.on("ready", createWindow);
