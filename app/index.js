a900f624e097f874d294928d9af23f047f7462ec
markxoe <mark@oude-elberink.de>
2020-10-18 20:21:37 +0200

    :sparkles: Click Bee to Rotate

--- app/index.js
+++ app/index.js
@@ -1,3 +1,16 @@
 const electron = require("electron").remote;
 
+const animejs = require("animejs");
+
 const closeWindow = electron.getCurrentWindow().close;
+
+const animation = animejs({
+    targets: "#biene",
+    rotate: "360",
+    duration: 1000,
+    endDelay: 0,
+    autoplay: false,
+});
+const animationRun = () => {
+    animation.restart();
+};
